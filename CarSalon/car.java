70863a929a79dd466678dcf0729d9c5c06f35d40
Magnetika <agneslilla.szabo@gmail.com>
2023-07-13 20:50:33 +0200

    Sololearn in the car salon solution

---
+++ CarSalon/car.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Demo{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int price = scanner.nextInt();
+        //complete the code
+        if(price  <=12000){System.out.println("enough");}
+
+    }
+}
