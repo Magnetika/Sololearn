c00b05e329fac9ad3b7862986c2485fbe30a2a16
Magnetika <agneslilla.szabo@gmail.com>
2023-07-13 20:47:57 +0200

    sololearn bar inverory

---
+++ Barinventory/bar.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void main(String[] args) {
+        int juice = 64;
+        int iceTea= 23;
+        //calculate the sum and output it
+            int total=juice +iceTea;
+            System.out.print(total);
+            
+	}
+}
